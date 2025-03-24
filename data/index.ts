317f85afcc5c172c3acda3d86c20603c6b37670c
Chirag Jain <chiragjain7300@gmail.com>
2025-03-24 14:31:10 +0530

    final commit

--- data/index.ts
+++ data/index.ts
@@ -72,24 +72,24 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
+    title: "PromptPedia",
+    des: "Next.js application that allows users to create, share, and manage prompts.",
+    img: "/Prompt.png",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    link: "https://prompt-pedia-seven.vercel.app/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    title: "TODO list app",
+    des: "Todo app with all CRUD operations, built with React and Tailwind CSS.",
+    img: "/todo.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
     link: "/ui.yoom.com",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    title: "Weather App",
+    des: "Weather app with real-time data, built with React and Tailwind CSS.",
     img: "/p3.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "/ui.aiimg.com",
@@ -173,29 +173,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Web Developer Intern",
+    desc: "Assisted in the development of a web-based platform using MERN stack, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Software Development Intern",
+    desc: "Designed and developed ERP software modules, enhancing user experience and performance using Oracle ADF at Mastek.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    desc: "Led the dev of a website for a client, from initial concept to deployment on web.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Associate Consultant at Mastek",
+    desc: "Developed and maintained ERP products using modern full stack technologies.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
@@ -205,13 +205,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://www.github.com/ChiragJain7300",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "#",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/chirag-jain-7300",
   },
 ];
